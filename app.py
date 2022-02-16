bda0209c799d276442dc01f115f46bf2fd613c53
Rick Jonkman <arjonkman@outlook.com>
2022-02-16 11:24:50 +0100

    Web Tecnologie Flask basis code

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/')
+def hello_world(name=None):
+    return render_template('index.html', name=name)
+
+@app.route('/noice')
+def noice(name=None):
+    return render_template('noice.html', name=name)
