032bef29fb92a7433a10c9e8d5eb2e07e2722f52
Rick Jonkman <arjonkman@outlook.com>
2022-04-17 23:46:03 +0200

    :smile:

--- project/frontend/src/Components/Home/index.tsx
+++ project/frontend/src/Components/Home/index.tsx
@@ -1,16 +1,17 @@
 import { Row, Col } from 'react-bootstrap'
 
+import stonks from './favicon.svg';
+
 function Home() {
 
   return (
     <Row className="pt-3 px-5 justify-content-center">
-		<Col xs={12} md={6}>
+		<Col className="text-center">
 			<h1><b>Paper-trading voor iedereen</b></h1>
 			<p>Wil jij zonder enige kosten leren aandelen te verhandelen op het internet?</p>
+			<p>Maak dan nu een account aan!</p>
 			<h3><b>BartStonks</b></h3>
-		</Col>
-		<Col xs={12} md={6}>
-
+			<img src={stonks} height={500} />
 		</Col>
 	</Row>
   )
